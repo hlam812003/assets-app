65b97d412ee931ad0ae330386485bac7577adde3
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-03-08 16:30:54 +0700

    Update Pinia Plugin, Typescript,...

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  pages: true,
+  typescript: {
+    strict: true,
+    typeCheck: true
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
@@ -10,5 +15,11 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
-  modules: ['@nuxt/ui']
+  modules: ['@nuxt/ui', '@pinia/nuxt', "@nuxt/image"],
+  pinia: {
+    storesDirs: ['./stores/**', './custom-folder/stores/**'],
+  },
+  imports: {
+    dirs: ['./stores']
+  }
 })
