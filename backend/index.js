e76a3cbad33ffac67322b2b58709efe3300cbc55
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-03-14 14:25:10 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,37 +1,3 @@
-// const app = require('./server');
-// const mysql = require('mysql2');
-
-// const PORT = process.env.PORT || 8080;
-
-// const pool = mysql.createPool({
-//     host: 'localhost',
-//     user: 'root',
-//     password: 'admin',
-//     database: 'assets-app',
-//     waitForConnections: true,
-//     connectionLimit: 10,
-//     queueLimit: 0
-// });
-// // Attempt to perform a simple query to the database to check the connection
-// // pool.query('SELECT * from authentication', (error, results, fields) => {
-// //     if (error) {
-// //         console.error('Error connecting to the database:', error);
-// //         return;
-// //     }
-// //     console.log('Successfully connected to the database.');
-// //     // Start the server only if the database connection is successful
-// //     app.listen(PORT, () => {
-// //         console.log(`Server is running on port ${PORT}`);
-// //     });
-// // });
-
-// app.listen(PORT, () => {
-//     // console.log(`${pool.user}`);
-//     // console.log(`${pool.password}`);
-//     // console.log(`${pool.database}`);
-//     console.log(`Server is running on port ${PORT}`);
-// });
-
 const express = require("express");
 const app = express();
 const port = 3000;
