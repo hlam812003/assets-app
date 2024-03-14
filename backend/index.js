9c885ea848ac7d4c169c93870c1bc634276a6425
NDK0045 <bimbimcute05@gmail.com>
2024-03-14 15:00:01 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 3001;
 const userRouter = require("./routes/user");
 const authenticationRouter = require("./routes/authentication")
 app.use(express.json());
