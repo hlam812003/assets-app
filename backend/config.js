15eb9ac546431b205e520b4b2ffa2134e94b5804
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-03-16 01:03:16 +0700

    Update

--- backend/config.js
+++ backend/config.js
@@ -3,8 +3,8 @@ const config = {
     /* don't expose password or any sensitive info, done only for demo */
     host: "localhost",
     user: "root",
-    password: "0345762405",
-    database: "assets-app",
+    password: "root",
+    database: "HighSchoolAssetManagement",
     connectTimeout: 60000
   },
   listPerPage: 10,
