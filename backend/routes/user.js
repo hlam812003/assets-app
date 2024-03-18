fd57e96e24dd1d21f5373dde7fb6af2d8ac37b52
powoftech <powoftech@gmail.com>
2024-03-19 01:34:58 +0700

    Refactor User routes and services

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,30 +1,30 @@
-const express = require("express");
-const router = express.Router();
-const user = require("../services/user");
+// const express = require("express");
+// const router = express.Router();
+// const user = require("../services/user");
 
-/* GET programming languages. */
-router.get("/", async function (req, res, next) {
-	try {
-		res.json(await user.getMultiple(req.query.page));
-	} catch (err) {
-		console.error(`Error while getting programming languages `, err.message);
-		next(err);
-	}
-});
+// /* GET programming languages. */
+// router.get("/", async function (req, res, next) {
+// 	try {
+// 		res.json(await user.getMultiple(req.query.page));
+// 	} catch (err) {
+// 		console.error(`Error while getting programming languages `, err.message);
+// 		next(err);
+// 	}
+// });
+
+// module.exports = router;
+
+const express = require("express");
+const UserService = require("../services/user");
+
+const router = express.Router();
+
+router.get("/:userId", UserService.getUser);
+
+router.post("/signup", UserService.signUp);
+
+router.post("/signin", UserService.signIn);
+
+router.post("/signout", UserService.signOut);
 
 module.exports = router;
-
-// import express from "express";
-// const UserService = require("../services/user");
-
-// const router = express.Router();
-
-// router.get("/", UsersController.getAuthenticatedUser);
-
-// router.post("/signup", UsersController.signUp);
-
-// router.post("/signin", UsersController.signIn);
-
-// router.post("/signout", UsersController.signOut);
-
-// export default router;
