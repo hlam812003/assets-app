1d0b097b7c673fe85ead165c403515c777a300b1
powoftech <powoftech@gmail.com>
2024-03-20 03:46:13 +0700

    Add admin routes/services prototype

---
+++ backend/routes/admin.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const requiresAuth = require("../middleware/auth");
+const AdminService = require("../services/admin");
+
+const router = express.Router();
+
+router.get("/user/", AdminService.getUsers);
+
+router.get("/user/:userId", AdminService.getUser);
+
+router.post("/user/", AdminService.createUser);
+
+router.patch("/user/:userId", AdminService.updateUser);
+
+router.delete("/user/:userId", AdminService.updateUser);
+
+module.exports = router;
