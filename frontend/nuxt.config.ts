44f01805620cd942a485e03302957d78e26331c0
powoftech <powoftech@gmail.com>
2024-03-21 04:27:41 +0700

    Setup proxy

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,28 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  pages: true,
-  css: ['~/assets/css/main.css', 'animate.css'],
-  components: {
-    global: true,
-    dirs: ['~/components']
-  },
-  postcss: {
-    plugins: {
-      'postcss-import': {},
-      'tailwindcss/nesting': 'postcss-nesting',
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  modules: [
-      '@nuxt/ui',
-      'nuxt-icon',
-      [
-        '@pinia/nuxt', { 
-          disableVuex: false,
-          autoImports: ['defineStore']
-        }
-    ],
-  ]
-})
+	devtools: { enabled: true },
+	// routeRules: {
+	// 	"/api/**": { proxy: "localhost:3001/**" },
+	// },
+	nitro: {
+		devProxy: {
+			"/api": {
+				target: "http://localhost:3001",
+				changeOrigin: true,
+				prependPath: true,
+			},
+		},
+	},
+	pages: true,
+	css: ["~/assets/css/main.css", "animate.css"],
+	components: {
+		global: true,
+		dirs: ["~/components"],
+	},
+	postcss: {
+		plugins: {
+			"postcss-import": {},
+			"tailwindcss/nesting": "postcss-nesting",
+			tailwindcss: {},
+			autoprefixer: {},
+		},
+	},
+	modules: [
+		"@nuxt/ui",
+		"nuxt-icon",
+		[
+			"@pinia/nuxt",
+			{
+				disableVuex: false,
+				autoImports: ["defineStore"],
+			},
+		],
+	],
+});
