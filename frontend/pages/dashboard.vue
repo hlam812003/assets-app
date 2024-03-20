eb8d53cc068fe64072a7bd43ff9637cedfe57bf1
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-03-21 01:33:59 +0700

    Update dashboard.vue

--- frontend/pages/dashboard.vue
+++ frontend/pages/dashboard.vue
@@ -21,7 +21,7 @@ useHead({
     title: 'Trang Quản Trị',
 });
 
-const activePageName = ref('DashboardUsers');
+const activePageName = ref('DashboardHome');
 
 const activePage = computed(() => {
   return resolveComponent(activePageName.value);
